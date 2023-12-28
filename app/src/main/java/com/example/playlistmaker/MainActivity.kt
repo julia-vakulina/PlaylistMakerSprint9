6002e08df25fa7694097316b2c8a4a3729fe1569
Julia <yuliavak@mail.ru>
2023-12-28 22:06:58 +0700

    commit 3

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -10,19 +10,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button_search = findViewById<Button>(R.id.button_search)
-        val button_media = findViewById<Button>(R.id.button_media)
-        val button_settings = findViewById<Button>(R.id.button_settings)
+        val buttonSearch = findViewById<Button>(R.id.button_search)
+        val buttonMedia = findViewById<Button>(R.id.button_media)
+        val buttonSettings = findViewById<Button>(R.id.button_settings)
 
-        button_search.setOnClickListener {
+        buttonSearch.setOnClickListener {
             val searchIntent = Intent(this, SearchActivity::class.java)
             startActivity(searchIntent)
         }
-        button_media.setOnClickListener {
+        buttonMedia.setOnClickListener {
             val mediaIntent = Intent(this, MediaActivity::class.java)
             startActivity(mediaIntent)
         }
-        button_settings.setOnClickListener {
+        buttonSettings.setOnClickListener {
             val settingsIntent = Intent(this, SettingsActivity::class.java)
             startActivity(settingsIntent)
         }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -2,6 +2,7 @@ package com.example.playlistmaker
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
@@ -11,11 +12,36 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-        val button_left = findViewById<Button>(R.id.button_left)
-        button_left.setOnClickListener {
+        val buttonLeft = findViewById<Button>(R.id.button_left)
+        val buttonShare = findViewById<Button>(R.id.button_share)
+        val buttonSupport = findViewById<Button>(R.id.button_support)
+        val buttonLegal = findViewById<Button>(R.id.button_legal)
+        buttonLeft.setOnClickListener {
             val leftIntent = Intent(this, MainActivity::class.java)
             startActivity(leftIntent)
-
+        }
+        buttonShare.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            val shareLink = R.string.share_link_text
+            shareIntent.putExtra(Intent.EXTRA_TEXT, shareLink)
+            shareIntent.type = "text/plain"
+            startActivity(shareIntent)
+        }
+        buttonSupport.setOnClickListener {
+            val message = R.string.support_text
+            val email = R.string.support_email
+            val subject = R.string.support_subject
+            val supportIntent = Intent(Intent.ACTION_SENDTO)
+            supportIntent.type = "text/plain"
+            supportIntent.data = Uri.parse("mailto:")
+            supportIntent.putExtra(Intent.EXTRA_EMAIL, email)
+            supportIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            supportIntent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(supportIntent)
+        }
+        buttonLegal.setOnClickListener {
+            val legalIntent = Intent(Intent.ACTION_VIEW, Uri.parse("https://yandex.ru/legal/practicum_offer/"))
+            startActivity(legalIntent)
         }
     }
 }
