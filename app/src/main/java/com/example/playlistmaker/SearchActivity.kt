59378a567d16c75f51368a8100bba7514ef50edc
Julia <yuliavak@mail.ru>
2024-01-11 06:38:06 +0700

    commit 4

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,13 +1,62 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
 
 class SearchActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        val editTextSearch = findViewById<EditText>(R.id.editTextSearch)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+        clearButton.setOnClickListener {
+            editTextSearch.setText("")
+        }
 
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+            override fun afterTextChanged(s: Editable?) {
+            }
+        }
+        editTextSearch.addTextChangedListener(simpleTextWatcher)
+        if (savedInstanceState != null) {
+            stringValue = savedInstanceState.getString(STRING_KEY, STRING_DEF)
+            editTextSearch.setText(stringValue)
+        }
+
+    }
+    var stringValue: String = STRING_DEF
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(STRING_KEY, stringValue)
+    }
+    companion object {
+        const val STRING_KEY = "1"
+        const val STRING_DEF = ""
+    }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        stringValue = savedInstanceState.getString(STRING_KEY, STRING_DEF)
+    }
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
     }
 }
